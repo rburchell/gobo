2870128801e163d3893086cf5d16782e14a53967
Robin Burchell <robin+git@viroteck.net>
2015-08-19 14:13:16 +0200

    Revert "irc: Use strings.Split, not regular expressions, to split words off a string."
    This reverts commit 80a6273eba5ec037c755b7b3d028dde9c15fc795.

--- irc.go
+++ irc.go
@@ -26,6 +26,7 @@ package main
 
 import "fmt"
 import "strings"
+import "regexp"
 
 // :cameron.freenode.net NOTICE * :*** Looking up your hostname...
 type Command struct {
@@ -66,8 +67,12 @@ func (this *Command) String() string {
 	return fmt.Sprintf("%s%s%s", prefix, this.Command, parameters)
 }
 
+var (
+	spacesExpr = regexp.MustCompile(` +`)
+)
+
 func splitArg(line string) (arg string, rest string) {
-	parts := strings.SplitN(line, " ", 2)
+	parts := spacesExpr.Split(line, 2)
 	if len(parts) > 0 {
 		arg = parts[0]
 	}
